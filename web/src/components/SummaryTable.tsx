9f9c62b6d7ffaec0ef12d0344ebaf87c0613185a
Gabrielly Maldonado <gabrielly.maldonadoo@gmail.com>
2023-02-26 04:41:33 -0300

    slots de dia

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,4 +1,12 @@
-const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
+
+import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning"
+import { HabitDay } from "./HabitDay"
+const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
+
+const summaryDates = generateDatesFromYearBeginning();
+const minimunSummaryDatesSize = 18 * 7;
+const amountOfDaysToFill = minimunSummaryDatesSize - summaryDates.length
+
 export function SummaryTable() {
 
     return (
@@ -10,6 +18,15 @@ export function SummaryTable() {
                             {weekDay}
                         </div>
                     )
+                })}
+            </div>
+            <div className="grid grid-rows-7 grid-flow-col gap-3">
+                <HabitDay />
+                {summaryDates.map(data => {
+                    return <HabitDay key={data.toString()} />
+                })}
+                {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {
+                    return <div key={i} className="w-10 h-10 bg-zinc-900 border border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"></div>
                 })}
             </div>
         </div>
