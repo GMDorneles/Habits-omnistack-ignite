86e5e36dc01f117c0ac5e188e6b8ff5dc35ca844
Gabrielly Maldonado <gabrielly.maldonadoo@gmail.com>
2023-02-27 03:50:55 -0300

    add header e confg tailwind

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,7 @@
-import { StyleSheet, Text, View,StatusBar } from 'react-native';
-import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from '@expo-google-fonts/inter';
+import { Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold, useFonts } from '@expo-google-fonts/inter';
+import { StatusBar, View } from 'react-native';
 import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold });
@@ -9,22 +10,11 @@ export default function App() {
     return (<Loading />)
   }
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
-      <StatusBar barStyle={"light-content"} backgroundColor={"transparent"} translucent={true}/>
+    <View>
+      <StatusBar barStyle={"light-content"} backgroundColor={"transparent"} translucent={true} />
+      <Home />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090A',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text:{
-    color:'#fff',
-    fontFamily:'Inter_800ExtraBold'
-  }
-});
+
