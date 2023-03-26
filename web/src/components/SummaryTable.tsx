dd8c5138b859b8a3292543109bdcc8adef5e5a3d
GABRIELLY MALDONADO DORNELES <49543498+GMDorneles@users.noreply.github.com>
2023-03-25 23:52:44 -0400

    feat modal  de informações  do dia

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -21,9 +21,8 @@ export function SummaryTable() {
                 })}
             </div>
             <div className="grid grid-rows-7 grid-flow-col gap-3">
-                <HabitDay />
                 {summaryDates.map(data => {
-                    return <HabitDay key={data.toString()} />
+                    return <HabitDay amount={5} completed={2} key={data.toString()} />
                 })}
                 {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {
                     return <div key={i} className="w-10 h-10 bg-zinc-900 border border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"></div>
